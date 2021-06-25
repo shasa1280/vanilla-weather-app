816efdad0c4cbc56d570ee7658a886f679b53a82
shasa1280 <s.boshoff@live.ca>
2021-06-25 15:39:41 -0400

    Created a new API call to get data for 7-day forecast

--- src/app.js
+++ src/app.js
@@ -22,7 +22,8 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Sat", "Sun", "Mon", "Tues", "Weds", "Thurs", "Fri"];
@@ -49,11 +50,14 @@ function displayForecast() {
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-
-  console.log(forecastHTML);
 }
-
-displayForecast();
+function getFutureForecast(coordinates) {
+  console.log(coordinates);
+  let apikey = "4c099f4f901402f4288ecadef6fa9e70";
+  let unit = "metric";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apikey}&units=${unit}`;
+  axios.get(apiURL).then(displayForecast);
+}
 
 function displayTemp(response) {
   let temperatureElement = document.querySelector("#temp");
@@ -81,6 +85,8 @@ function displayTemp(response) {
     ` http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   weatherIconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getFutureForecast(response.data.coord);
 }
 
 function search(city) {
